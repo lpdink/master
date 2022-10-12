535d3ba52c2f770d794ffcf18eacd4dd36bb847b
lpdink <lpdink@qq.com>
2022-10-12 13:27:07 +0000

    pytorch_introduction update chapter1 img and NL preprocess

---
+++ book/pytorch_introduction/chapter1/NL_preprocess.py
@@ -0,0 +1,22 @@
+# 中文分词的常见方法是THULAC和Jieba
+# 本P演示英文的word to vec过程。(使用Numpy 正态分布)
+from math import sqrt
+import numpy as np
+
+with open("./resources/text.txt", encoding="utf-8") as file:
+    TEXT = file.read()
+    TEXT = TEXT.replace(",","").replace("′", "").replace("?", "").replace("(", "").replace(")", "").replace('"', '')
+
+def get_word_vec(word_bag):
+    # 有n个单词，每个单词对应m长度的向量，n与m的关系，依照经验满足：m的大小与n的四次方根成正比，比例系数1-10，且为2的整数次方
+    n = len(word_bag) # n=117，四次方根约等于3.2，取8作为m的长度
+    m = 8
+    word2vec = np.random.normal(size=(n, m)).tolist()
+    word2vec = {word:vec for word, vec in zip(word_bag, word2vec)}
+    return word2vec
+
+if __name__=="__main__":
+    word_bag = list({word for lines in TEXT.split("\n") for word in lines.split()})
+    word2vec = get_word_vec(word_bag)
+    print(word2vec)
+    breakpoint()
--- book/pytorch_introduction/chapter1/image_preprocess.py
+++ book/pytorch_introduction/chapter1/image_preprocess.py
@@ -1,15 +1,21 @@
 # 读取图像到[图像高，图像宽，通道]
 # conda install Pillow
+# 对hsv改变幅度，且不超出范围，以进行数据增强。
 from functools import reduce
 from PIL import Image
 import numpy as np
 import os
+import random
 
 RAW_PATH = "./resources/img_raw"
 PREPARED_PATH = "./resources/img_prepared"
 
 TARGET_SIZE = (300, 300)
 FILTER = Image.NEAREST
+
+# 是否进行增强
+ENHANCE = True
+RANDOM_RANGE = 0.5 # 增强10%
 
 
 def resize_jpgs(dir_path=RAW_PATH):
@@ -22,6 +28,28 @@ def resize_jpgs(dir_path=RAW_PATH):
     for jpg_path in jpgs:
         im = Image.open(jpg_path)
         im = im.resize(TARGET_SIZE, FILTER)
+        if ENHANCE:
+            im_hsv = im.convert("HSV")
+            width, height = im_hsv.size
+            pixel_map = im_hsv.load()
+            for w in range(width):
+                for h in range(height):
+                    pix_h, pix_s, pix_v = im_hsv.getpixel((w, h))
+                    pixel_map[w, h] = (int(min(pix_h*(1+RANDOM_RANGE), 255)), 
+                                        int(min(pix_s*(1+RANDOM_RANGE), 255)),
+                                        int(min(pix_v*(1+RANDOM_RANGE), 255)))
+            im = im_hsv.convert("RGB")
+                    
+        """
+        在opencv中的范围如下，但在PIL中，H似乎也是255的。
+        H = [0,179]
+        L = [0,255]
+        S = [0,255]
+
+        H = [0,179]
+        S = [0,255]
+        V = [0,255]
+        """
         im.save(os.path.join(PREPARED_PATH, "prepared_" + os.path.basename(jpg_path)))
         im_np = np.array(im)
         # 在axis=0升一维，表示文件索引
@@ -34,4 +62,3 @@ def resize_jpgs(dir_path=RAW_PATH):
 if __name__ == "__main__":
     im_nps = resize_jpgs()
     print(im_nps.shape)
-    breakpoint()
