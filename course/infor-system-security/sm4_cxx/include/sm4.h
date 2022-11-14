8969ba470f8662efc76b9b080c5475a9445d480e
lpdink <lpdink@qq.com>
2022-11-14 03:47:54 +0000

    [fix] update sm4.h to outside

--- course/infor-system-security/sm4_cxx/include/sm4.h
+++ course/infor-system-security/sm4_cxx/include/sm4.h
@@ -1,8 +1,17 @@
+#ifndef SM4_H
+#define SM4_H
 #include <memory.h>
 
 #include <string>
 namespace sm4 {
+#ifdef __cplusplus
+extern "C" {
+#endif
 void encrypt(std::string &key, std::string &msg, unsigned char *rst);
 
 std::string decrypt(std::string &key, unsigned char *rst, int num);
+#ifdef __cplusplus
+}
+#endif
 }  // namespace sm4
+#endif
