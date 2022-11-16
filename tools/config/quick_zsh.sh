e8911f424a23357f0daee918bb4830ca251380d6
lpdink <lpdink@qq.com>
2022-11-16 18:38:21 +0800

    [fix] fix quick zsh bug

--- tools/config/quick_zsh.sh
+++ tools/config/quick_zsh.sh
@@ -1,7 +1,7 @@
 # 请手动下载并安装字体文件到本地计算机！
 # https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 # https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-# https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+# https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 # https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 # 快速配置zsh
@@ -10,7 +10,6 @@ zsh_path=$(which zsh)
 if [ $? -ne 0 ];then
 echo "without zsh, prepare to install."
 sudo apt-get install zsh
-cp ./zshrc.sh $HOME"/.zshrc"
 fi
 # 切换zsh为默认终端
 chsh -s $zsh_path
@@ -18,7 +17,7 @@ chsh -s $zsh_path
 # 检查oh my zsh安装情况
 if [ ! -e $HOME"/.oh-my-zsh" ];then
 echo "without oh-my-zsh, prepare to install"
-sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+sh -c "$(wget -O- https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
 fi
 # 检查p10k安装情况
 p10k_path=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -28,5 +27,8 @@ git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git $p10k_path
 fi
 # 拷贝主题配置
 cp ./p10k.zsh $HOME"/.p10k.zsh"
+cp ./zshrc.sh $HOME"/.zshrc"
 echo "zsh config done."
 echo "Use 'zsh' to activate."
+# 如果在vscode中使用，搜索terminal.integrated.fontFamily，修改为MesloLGS NF
+# 如果是windows终端，修改字体为MesloLGS NF
